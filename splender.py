1fd1c4160db5c24fc49211df1e69bb5a435aa2d0
artitsamchooosri <33492904+artitsamchooosri@users.noreply.github.com>
2022-09-03 13:07:11 +0700

    f2

---
+++ splender.py
@@ -0,0 +1,301 @@
+import numpy as np
+import random
+from collections import defaultdict
+import json
+import csv
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+class splender :
+    def __init__(self):
+        def csv_to_dict(filename):
+            result_list=[]
+            with open(filename) as file_obj:
+                reader = csv.DictReader(file_obj)
+                for row in reader:
+                    result_list.append(dict(row))
+            return result_list
+        self.gem=[4,4,4] #red,blue,green
+        self.card = csv_to_dict('card.csv')
+        self.old_open_used = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.open_used = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.open_card = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.card_p1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.card_p2 =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.score_P1 = 0
+        self.score_P2 = 0
+        self.gem_P1=[0,0,0]
+        self.gem_P2=[0,0,0]
+        self.gem_bonus_P1=[0,0,0] #red,blue,green
+        self.gem_bonus_P2=[0,0,0] #red,blue,green
+    
+    def show_field(self):
+        def check_free(red,blue,green):
+            if red == "1":
+                return "RED"
+            elif blue =="1":
+                return "BLUE"
+            elif green =="1":
+                return "GREEN"
+            else:
+                return "NULL"
+        print("the remaining gems\nred = {} blue = {} green = {}".format(self.gem[0],self.gem[1],self.gem[2]))
+        print("------------------------------")
+        print("Cards on the field")
+        print("Level 1 : ")
+        for i in range(len(self.open_card)):
+            if self.open_card[i] == 1:
+                if self.card[i]["level"] == "1":
+                    freegem=check_free(self.card[i]["red_free"],self.card[i]["blue_free"],self.card[i]["green_free"])
+                    print("CARD {} || PAY red = {} blue = {} green = {} || Bonus = {}  || Score = {}".format(self.card[i]["namecard"],self.card[i]["red_buy"],self.card[i]["blue_buy"],self.card[i]["green_buy"],freegem,self.card[i]["score"]))
+        print("------------------------------")
+        print("Level 2 : ")
+        for i in range(len(self.open_card)):
+            if self.open_card[i] == 1:
+                if self.card[i]["level"] == "2":
+                    freegem=check_free(self.card[i]["red_free"],self.card[i]["blue_free"],self.card[i]["green_free"])
+                    print("CARD {} || PAY red = {} blue = {} green = {} || Bonus = {}  || Score = {}".format(self.card[i]["namecard"],self.card[i]["red_buy"],self.card[i]["blue_buy"],self.card[i]["green_buy"],freegem,self.card[i]["score"]))
+        print("------------------------------")
+        print("Level 3 : ")
+        for i in range(len(self.open_card)):
+            if self.open_card[i] == 1:
+                if self.card[i]["level"] == "3":
+                    freegem=check_free(self.card[i]["red_free"],self.card[i]["blue_free"],self.card[i]["green_free"])
+                    print("CARD {} || PAY red = {} blue = {} green = {} || Bonus = {}  || Score = {}".format(self.card[i]["namecard"],self.card[i]["red_buy"],self.card[i]["blue_buy"],self.card[i]["green_buy"],freegem,self.card[i]["score"]))
+        print("-------------Score--------------")
+        print("score of the 1st player : ",self.score_P1)
+        print("score of the 2st player : ",self.score_P2)
+        print("USED CARD : {}".format(self.open_used))
+        print("OPEN CARD : {}".format(self.open_card))
+        print("------------- 1st player--------------")
+        print("GEM:: RED = {} BLUE = {} GREEN = {}".format(str(self.gem_P1[0]),str(self.gem_P1[1]),str(self.gem_P1[2])))
+        print("GEM BONUS :: RED = {} BLUE = {} GREEN = {}".format(str(self.gem_bonus_P1[0]),str(self.gem_bonus_P1[1]),str(self.gem_bonus_P1[2])))
+        print("CARD {}".format(self.card_p1))
+        print("------------- 2st player--------------")
+        print("GEM:: RED = {} BLUE = {} GREEN = {}".format(str(self.gem_P2[0]),str(self.gem_P2[1]),str(self.gem_P2[2])))
+        print("GEM BONUS :: RED = {} BLUE = {} GREEN = {}".format(str(self.gem_bonus_P2[0]),str(self.gem_bonus_P2[1]),str(self.gem_bonus_P2[2])))
+        print("CARD {}".format(self.card_p2))
+        print("-------------------------------------")
+    def action_gem(self, player, choose):
+        def getmaxgem():
+            gemboard=[0,0,0]
+            gemweight=[0,0,0]
+            for i in range(len(self.open_card)):
+                if self.open_card[i] == 1:
+                    gemboard[0]+=int(self.card[i]["red_buy"])
+                    gemboard[1]+=int(self.card[i]["blue_buy"])
+                    gemboard[2]+=int(self.card[i]["green_buy"])
+            total = np.sum(gemboard)
+            gemweight[0]=gemboard[0]/total
+            gemweight[1]=gemboard[1]/total
+            gemweight[2]=gemboard[2]/total
+            #print(gemweight[0])
+            #print(gemweight[1])
+            #print(gemweight[2])
+            return random.choices(population=[0,1,2],weights=gemweight)
+        def getmingem():
+            gemboard=[0,0,0]
+            gemweight=[0,0,0]
+            for i in range(len(self.open_card)):
+                if self.open_card[i] == 1:
+                    gemboard[0]+=int(self.card[i]["red_buy"])
+                    gemboard[1]+=int(self.card[i]["blue_buy"])
+                    gemboard[2]+=int(self.card[i]["green_buy"])
+            total = np.sum(gemboard)
+            gemweight[0]=1-(gemboard[0]/total)
+            gemweight[1]=1-(gemboard[1]/total)
+            gemweight[2]=1-(gemboard[2]/total)
+            #print(gemweight[0])
+            #print(gemweight[1])
+            #print(gemweight[2])
+            return random.choices(population=[0,1,2],weights=gemweight)
+        if (np.sum(self.gem)<2) :
+            print("Not enough gems")
+            return False
+        else:
+            #------------------------------------- ทิ้ง gem
+            if player==1 and np.sum(self.gem_P1)>=5 and choose=="random":
+                i=0
+                while i <= 1:
+                    pickgem=getmingem()
+                    if self.gem_P1[pickgem[0]]-1>=0:
+                        self.gem_P1[pickgem[0]]-=1
+                        self.gem[pickgem[0]]=self.gem[pickgem[0]]+1
+                        i=i+1
+                        print("ทิ้ง GEM {}".format(pickgem[0]))
+                        #print("the เหลือ\nred = {} blue = {} green = {}".format(self.gem[0],self.gem[1],self.gem[2]))
+                        #print("the มี\nred = {} blue = {} green = {}".format(self.gem_P1[0],self.gem_P1[1],self.gem_P1[2]))
+            elif player==2 and np.sum(self.gem_P2)>=5 and choose=="random":
+                i=0
+                while i <= 1:
+                    pickgem=getmingem()
+                    if self.gem_P2[pickgem[0]]-1>=0:
+                        self.gem_P2[pickgem[0]]-=1
+                        self.gem[pickgem[0]]=self.gem[pickgem[0]]+1
+                        i=i+1
+                        print("ทิ้ง GEM {}".format(pickgem[0]))
+                        #print("the เหลือ\nred = {} blue = {} green = {}".format(self.gem[0],self.gem[1],self.gem[2]))
+                        #print("the มี\nred = {} blue = {} green = {}".format(self.gem_P2[0],self.gem_P2[1],self.gem_P2[2]))
+            #------------------------------------- เลือก gem
+            if player==1 and choose=="random":
+                i=0
+                while i <= 1:
+                    pickgem=getmaxgem()
+                    if self.gem[pickgem[0]]-1>=0:
+                        self.gem_P1[pickgem[0]]+=1
+                        self.gem[pickgem[0]]=self.gem[pickgem[0]]-1
+                        i=i+1
+                        print("เลือก GEM {}".format(pickgem[0]))
+                        #print("เหลือ\nred = {} blue = {} green = {}".format(self.gem[0],self.gem[1],self.gem[2]))
+                        #print(" มี\nred = {} blue = {} green = {}".format(self.gem_P1[0],self.gem_P1[1],self.gem_P1[2]))
+            elif player==2 and choose=="random":
+                i=0
+                while i <= 1:
+                    pickgem=getmaxgem()
+                    if self.gem[pickgem[0]]-1>=0:
+                        self.gem_P2[pickgem[0]]+=1
+                        self.gem[pickgem[0]]=self.gem[pickgem[0]]-1
+                        i=i+1
+                        print("เลือก GEM {}".format(pickgem[0]))
+                        #print("เหลือ\nred = {} blue = {} green = {}".format(self.gem[0],self.gem[1],self.gem[2]))
+                        #print("มี\nred = {} blue = {} green = {}".format(self.gem_P2[0],self.gem_P2[1],self.gem_P2[2]))
+    def action_buy(self, player, choose):
+        gem_player=[0,0,0]
+        if player==1:
+            gem_player[0]+=self.gem_P1[0]+self.gem_bonus_P1[0]
+            gem_player[1]+=self.gem_P1[1]+self.gem_bonus_P1[1]
+            gem_player[2]+=self.gem_P1[2]+self.gem_bonus_P1[2]
+        elif player==2:
+            gem_player[0]+=self.gem_P2[0]+self.gem_bonus_P2[0]
+            gem_player[1]+=self.gem_P2[1]+self.gem_bonus_P2[1]
+            gem_player[2]+=self.gem_P2[2]+self.gem_bonus_P2[2]
+        if self.open_used[choose]==0 and self.open_card[choose]==1:
+            pay_gem=[int(self.card[choose]["red_buy"]),int(self.card[choose]["blue_buy"]),int(self.card[choose]["green_buy"])]
+            print(pay_gem)
+            pay_gem[0]=pay_gem[0]-gem_player[0]
+            pay_gem[1]=pay_gem[1]-gem_player[1]
+            pay_gem[2]=pay_gem[2]-gem_player[2]
+            if pay_gem[0]<=0 and pay_gem[1] <=0 and pay_gem[2] <=0:
+                self.open_card[choose]=0
+                self.old_open_used=self.open_used.copy()
+                self.open_used[choose]=1
+                if player==1:
+                    self.card_p1[choose]=1
+                    difgem=[0,0,0]
+                    difgem[0]=self.gem_bonus_P1[0]-int(self.card[choose]["red_buy"])
+                    if difgem[0]<0:
+                        self.gem_P1[0]=self.gem_P1[0]+difgem[0]
+                        self.gem[0]-=difgem[0]
+
+                    difgem[1]=self.gem_bonus_P1[1]-int(self.card[choose]["blue_buy"])
+                    if difgem[1]<0:
+                        self.gem_P1[1]=self.gem_P1[1]+difgem[1]
+                        self.gem[1]-=difgem[1]
+
+                    difgem[2]=self.gem_bonus_P1[2]-int(self.card[choose]["green_buy"])
+                    if difgem[2]<0:
+                        self.gem_P1[2]=self.gem_P1[2]+difgem[2]
+                        self.gem[2]-=difgem[2]
+
+                    self.gem_bonus_P1[0]+=int(self.card[choose]["red_free"])
+                    self.gem_bonus_P1[1]+=int(self.card[choose]["blue_free"])
+                    self.gem_bonus_P1[2]+=int(self.card[choose]["green_free"])
+                    self.score_P1+=int(self.card[choose]["score"])
+                    #print(pay_gem)
+                    print("BUY {}".format(self.card[choose]["namecard"]))
+                    #print("the gems\nred = {} blue = {} green = {}".format(self.gem_P1[0],self.gem_P1[1],self.gem_P1[2]))
+                    #print("the bonus\nred = {} blue = {} green = {}".format(self.gem_bonus_P1[0],self.gem_bonus_P1[1],self.gem_bonus_P1[2]))
+                elif player==2:
+                    self.card_p2[choose]=1
+                    difgem=[0,0,0]
+
+                    difgem[0]=self.gem_bonus_P2[0]-int(self.card[choose]["red_buy"])
+                    if difgem[0]<0:
+                        self.gem_P2[0]=self.gem_P2[0]+difgem[0]
+                        self.gem[0]-=difgem[0]
+
+                    difgem[1]=self.gem_bonus_P2[1]-int(self.card[choose]["blue_buy"])
+                    if difgem[1]<0:
+                        self.gem_P2[1]=self.gem_P2[1]+difgem[1]
+                        self.gem[1]-=difgem[1]
+
+                    difgem[2]=self.gem_bonus_P2[2]-int(self.card[choose]["green_buy"])
+                    if difgem[2]<0:
+                        self.gem_P2[2]=self.gem_P2[2]+difgem[2]
+                        self.gem[2]-=difgem[2]
+
+                    self.gem_bonus_P2[0]+=int(self.card[choose]["red_free"])
+                    self.gem_bonus_P2[1]+=int(self.card[choose]["blue_free"])
+                    self.gem_bonus_P2[2]+=int(self.card[choose]["green_free"])
+                    self.score_P2+=int(self.card[choose]["score"])
+                    print("BUY {}".format(self.card[choose]["namecard"]))
+            else:
+                return False
+        else:
+            return False
+    def open_card_buy(self):
+        l1=0
+        l2=0
+        l3=0
+        filtter_usercard_l1=[]
+        filtter_usercard_l2=[]
+        filtter_usercard_l3=[]
+        for i in range(len(self.open_card)):
+            if self.open_card[i]==1:
+                if self.card[i]["level"] == "1":
+                    l1+=1
+                elif self.card[i]["level"] == "2":
+                    l2+=1
+                elif self.card[i]["level"] == "3":
+                    l3+=1
+            else:
+                if self.card[i]["level"] == "1":
+                    if self.open_used[i] ==0:
+                        filtter_usercard_l1.append(i) 
+                elif self.card[i]["level"] == "2":
+                    if self.open_used[i] ==0:
+                        filtter_usercard_l2.append(i)
+                elif self.card[i]["level"] == "3":
+                    if self.open_used[i] ==0:
+                        filtter_usercard_l3.append(i)    
+        while l1 <= 4:
+            if(len(filtter_usercard_l1)>0):
+                x = random.choice(filtter_usercard_l1)
+                self.open_card[x]=1
+                l1+=1
+            else:
+                break
+        while l2 <= 4:
+            if(len(filtter_usercard_l2)>0):
+                x = random.choice(filtter_usercard_l2)
+                self.open_card[x]=1
+                l2+=1
+            else:
+                break
+        while l3 <= 4:
+            if(len(filtter_usercard_l3)>0):
+                x = random.choice(filtter_usercard_l3)
+                self.open_card[x]=1
+                l3+=1
+            else:
+                break
+    def check_buy_card(self, player):
+        list_card_buy=[]
+        gem_player=[0,0,0]
+        if player==1:
+            gem_player[0]+=self.gem_P1[0]+self.gem_bonus_P1[0]
+            gem_player[1]+=self.gem_P1[1]+self.gem_bonus_P1[1]
+            gem_player[2]+=self.gem_P1[2]+self.gem_bonus_P1[2]
+        elif player==2:
+            gem_player[0]+=self.gem_P2[0]+self.gem_bonus_P2[0]
+            gem_player[1]+=self.gem_P2[1]+self.gem_bonus_P2[1]
+            gem_player[2]+=self.gem_P2[2]+self.gem_bonus_P2[2]
+        for i in range(len(self.open_card)):
+            if self.open_card[i] == 1:
+                pay_gem=[int(self.card[i]["red_buy"]),int(self.card[i]["blue_buy"]),int(self.card[i]["green_buy"])]
+                #print(pay_gem)     
+                pay_gem[0]=pay_gem[0]-gem_player[0]
+                pay_gem[1]=pay_gem[1]-gem_player[1]
+                pay_gem[2]=pay_gem[2]-gem_player[2]
+                if pay_gem[0]<=0 and pay_gem[1] <=0 and pay_gem[2] <=0:
+                    list_card_buy.append(i)
+        return list_card_buy
---
+++ tran_bot.py
@@ -0,0 +1,105 @@
+import os
+from IPython.display import clear_output
+import time 
+import math
+import numpy as np
+import random
+from collections import defaultdict
+import json
+import csv
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+from splender import splender
+clear = lambda: os.system('cls')
+type_play=1
+def random_play():
+        q = defaultdict(lambda: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+        rewards = np.array([])
+        games = [splender() for i in range(2)]
+        #     Hyperparameters
+        epsilon = 1
+        epsilon_min = 0.05
+        epsilon_decay = 0.999997
+        gamma = 0.9
+        alpha = 0.1
+        def updateQ(s, a, new_s, r):
+                q_value = q[str(s)].copy()
+                q_value_a=q_value[a]
+                #print("q_value_a : {}".format(q_value_a))
+                max_q=max(q[str(new_s)])
+                #print("max_q : {}".format(max_q))
+                #print("r : {}".format(r))
+                q_value[a] = ((1 - alpha) * q_value_a) + (alpha * (r + (gamma * max_q)))
+                #print("q_value : {}".format(q_value[a]))
+                q[str(s)] = q_value
+        for game in games:
+                Round = 1
+                player=0
+                game.open_card_buy()
+                while(True):
+
+                        clear_output(wait=True)
+                        clear()
+                        if Round%2 == 0 :
+                                player = 2
+                        elif Round%2 == 1 :
+                                player = 1
+                        print("Round : {} || Player : {}".format(math.ceil(Round/2),player))
+                        print("Score_P1 : {}".format(game.score_P1))
+                        print("Score_P2 : {}".format(game.score_P2))
+                        game.open_card_buy()
+                        if(game.score_P1>=10):
+                                time.sleep(5)
+                                clear()
+                                clear_output(wait=True)
+                                print("Round : {}".format(math.ceil(Round/2)))
+                                print("Score_P1 : {}".format(game.score_P1))
+                                print("Score_P2 : {}".format(game.score_P2))
+                                print("Player 1 is Win")
+                                break
+                        elif game.score_P2>=10:
+                                time.sleep(5)
+                                clear()
+                                clear_output(wait=True)
+                                print("Round : {}".format(math.ceil(Round/2)))
+                                print("Score_P1 : {}".format(game.score_P1))
+                                print("Score_P2 : {}".format(game.score_P2))
+                                print("Player 2 is Win")
+                                break
+                        if type_play == 1 :
+                                print("Bot")
+                                listaction=[]
+                                listaction.append("GEM")
+                                list_buy_card=game.check_buy_card(player)
+                                print(len(list_buy_card))
+                                #game.show_field()
+                                if(len(list_buy_card)>0):
+                                        listaction.append("BUY")
+                                else:
+                                        listaction.append("GEM")
+                                print(listaction)
+                                action=random.choices(population=listaction,weights=[0.2,0.8])
+                                if(action[0]=="BUY"):
+                                        id_buy=random.choice(list_buy_card)
+                                        game.action_buy(player,id_buy)
+                                        if not(np.array_equal(game.old_open_used, game.open_used)):
+                                                if player==1:
+                                                        updateQ(game.old_open_used, id_buy, game.open_used, game.score_P1)
+                                                elif player==2:
+                                                        updateQ(game.old_open_used, id_buy, game.open_used, game.score_P1)
+                                        #sp.show_field()
+                                else:
+                                        game.action_gem(player,"random")
+                                #print("Player {} Action {} ".format(str(player),action[0]))
+                                Round+=1
+                                #time.sleep(5)
+                                #clear_output(wait=True)
+                                #print("Round : {} Player : {} || ".format(math.ceil(Round/2),player))
+                                #print("Player {} Last Action {} ".format(str(player),action[0]))
+                                #sp.show_field()
+                                time.sleep(1)
+        fo = open("qtablespen.json", "w")
+        json.dump(q, fo)
+        fo.close()
+        return q
+random_play()
